88cadb5b05964a42c1540ea795b5e04501f4d153
Sumanth <usumanth@deloitte.com>
2026-01-15 19:37:04 +0530

    test vercel

--- Client/src/store/hooks.js
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-
-// Typed hooks for TypeScript-like usage
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = useSelector;
